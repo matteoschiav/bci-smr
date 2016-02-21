d4fa3dc71648fc64fcad8b7ef2d1e16d451fef27
Schiavon <matteoschiav@yahoo.it>
2016-02-22 00:47:26 +0100

    Compute PSD

--- bci_demo.m
+++ bci_demo.m
@@ -3,10 +3,11 @@ rootpath = '/home/schiavon/Matteo/luxor/cisas/bci/';
 trainpath = [rootpath 'bci4neurorobotics-data/smr/20150914_b1/'];
 codepath = [rootpath 'bci4neurorobotics-code/'];
 
-oldfolder = cd([codepath 'biosig']);
-biosig_installer;
-cd(oldfolder);
+% oldfolder = cd([codepath 'biosig']);
+% biosig_installer;
+% cd(oldfolder);
 
 filetrain{1} = 'b1.20150914.102659.offline.mi.mi_rlbf.gdf';
 
 offline = bci(filetrain{1},trainpath,codepath);
+offline = offline.compute_PSD();
