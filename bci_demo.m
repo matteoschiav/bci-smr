1d8107ffd7615095ceae117e69078610954cf52b
Schiavon <matteoschiav@yahoo.it>
2016-02-28 18:26:06 +0100

    Analyse data of final report

--- bci_demo.m
+++ bci_demo.m
@@ -4,7 +4,7 @@ close all
 clear classes
 
 rootpath = '/home/schiavon/Matteo/luxor/cisas/bci/';
-% trainpath = [rootpath 'bci4neurorobotics-data/smr/20150914_b1/'];
+trainpath = [rootpath 'bci4neurorobotics-data/smr/20150914_b1/'];
 codepath = [rootpath 'bci4neurorobotics-code/'];
 
 % oldfolder = cd([codepath 'biosig']);
@@ -12,16 +12,16 @@ codepath = [rootpath 'bci4neurorobotics-code/'];
 % cd(oldfolder);
 
 % classifier train
-% filetrain{1} = 'b1.20150914.102659.offline.mi.mi_rlbf.gdf';
-% filetrain{2} = 'b1.20150914.104000.offline.mi.mi_rlbf.gdf';
-% filetrain{3} = 'b1.20150914.105054.offline.mi.mi_rlbf.gdf';
+filetrain{1} = 'b1.20150914.102659.offline.mi.mi_rlbf.gdf';
+filetrain{2} = 'b1.20150914.104000.offline.mi.mi_rlbf.gdf';
+filetrain{3} = 'b1.20150914.105054.offline.mi.mi_rlbf.gdf';
 
 % TEST DATA
-trainpath = [rootpath 'bci4neurorobotics-data/smr/20150831_b4/'];
-
-filetrain{1} = 'b4.20150831.121033.offline.mi.mi_rlsf.gdf';
-filetrain{2} = 'b4.20150831.123916.offline.mi.mi_rlsf.gdf';
-filetrain{3} = 'b4.20150831.125323.offline.mi.mi_rlsf.gdf';
+% trainpath = [rootpath 'bci4neurorobotics-data/smr/20150831_b4/'];
+% 
+% filetrain{1} = 'b4.20150831.121033.offline.mi.mi_rlsf.gdf';
+% filetrain{2} = 'b4.20150831.123916.offline.mi.mi_rlsf.gdf';
+% filetrain{3} = 'b4.20150831.125323.offline.mi.mi_rlsf.gdf';
 % filetrain{4} = 'b4.20150831.132126.offline.mi.mi_rlsf.gdf';
 % END TEST DATA
 
